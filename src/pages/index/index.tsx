25c15391382839c77a05a0a8936d6281c243c1fc
张玉 <zyjavascript@163.com>
2021-08-24 12:29:11 +0800

    test: docker

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -79,7 +79,7 @@ class Index extends Component {
           <Text>{this.props.counter.num}</Text>
         </View>
         <View>
-          <Text>Hello Worl</Text>
+          <Text>Hello Wrl</Text>
         </View>
       </View>
     );
